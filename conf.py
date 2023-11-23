d436e09be53c04599a7de0fd466307385ef3c4b2
keyestudio <szkeyes@outlook.com>
2023-11-23 11:17:31 +0800

    main

--- conf.py
+++ conf.py
@@ -17,7 +17,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'keyestudio WiKi'
+project = 'ESP32 Starter Kit'
 copyright = 'keyestudio , WiKi'
 author = 'keyestudio WiKi'
 
